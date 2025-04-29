4743eb00e038a38982a5915aeb3099179fabcd46
Joshua <joshuachinxj289@gmail.com>
2025-04-29 15:58:15 +0800

    Revert "#9 Feature/sample leaderboard data"

--- app/routes/visualise_routes.py
+++ app/routes/visualise_routes.py
@@ -1,19 +1,9 @@
 # app/routes/visualise_routes.py: Routes for visualising data
 
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, render_template
 
 visualise_bp = Blueprint('visualise', __name__, url_prefix='/visualise')
 
 @visualise_bp.route('/')
 def visualise():
     return render_template('visualise.html')
-
-
-@visualise_bp.route('/api/study-time-data')
-def study_time_data():
-    data = [
-        {"date": "2025-04-01", "studyTime": 2},
-        {"date": "2025-04-02", "studyTime": 3},
-        {"date": "2025-04-03", "studyTime": 1.5},
-    ]
-    return jsonify(data)
