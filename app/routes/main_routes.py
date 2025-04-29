30cb8d02a191a17abba86a676792d08377afd604
Jaecheonz <joshuachinxj289@gmail.com>
2025-04-29 15:21:04 +0800

    removed obsolete session imports from the routes files

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -1,5 +1,5 @@
 # app/routes/main_routes.py
-from flask import Blueprint, render_template, redirect, url_for, session
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import current_user
 from flask_wtf import FlaskForm
 from wtforms import SubmitField
