e4719707383d8e2f52e1da5213c26fa2c4e234b2
marc-la <23857377@student.uwa.edu.au>
2025-05-04 19:27:38 +0800

    Fixed environment variable loading from .env in root directory for proper funcitonality

--- app/__init__.py
+++ app/__init__.py
@@ -13,8 +13,7 @@ csrf = CSRFProtect()
 login_manager = LoginManager()
 
 def create_app():
-    app = Flask(__name__)
-
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(Config)
 
     db.init_app(app)
--- instance/config.py
+++ instance/config.py
@@ -1,7 +1,9 @@
 # instance/config.py: # Configuration file
 import os
 from dotenv import load_dotenv
-load_dotenv()
+
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+load_dotenv(dotenv_path)
 
 # AT ALL COST REMOVE THESE DEFAULT VALUES FOR SUBMISSION
 class Config:
